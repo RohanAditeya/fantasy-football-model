7974905269a212dd38e289677ba03fa8bd6b7402
Rohan Aditeya <rohan.aditeya@gmail.com>
2023-12-10 18:43:44 +0530

    Added player and team basic info.

---
+++ src/main/java/com/fantasy/football/model/LeagueTeam.java
@@ -0,0 +1,193 @@
+package com.fantasy.football.model;
+
+public class LeagueTeam {
+    private Integer code;
+    private Integer draw;
+    private Float form;
+    private Integer id;
+    private Integer loss;
+    private String name;
+    private Integer played;
+    private Integer points;
+    private Integer position;
+    private String shortName;
+    private Integer strength;
+    private String teamDivision;
+    private Boolean unavailable;
+    private Integer win;
+    private Integer strengthOverallHome;
+    private Integer strengthOverallAway;
+    private Integer strengthAttackHome;
+    private Integer strengthAttackAway;
+    private Integer strengthDefenceHome;
+    private Integer strengthDefenceAway;
+    private Integer pulseId;
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
+    public Integer getDraw() {
+        return draw;
+    }
+
+    public void setDraw(Integer draw) {
+        this.draw = draw;
+    }
+
+    public Float getForm() {
+        return form;
+    }
+
+    public void setForm(Float form) {
+        this.form = form;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getLoss() {
+        return loss;
+    }
+
+    public void setLoss(Integer loss) {
+        this.loss = loss;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getPlayed() {
+        return played;
+    }
+
+    public void setPlayed(Integer played) {
+        this.played = played;
+    }
+
+    public Integer getPoints() {
+        return points;
+    }
+
+    public void setPoints(Integer points) {
+        this.points = points;
+    }
+
+    public Integer getPosition() {
+        return position;
+    }
+
+    public void setPosition(Integer position) {
+        this.position = position;
+    }
+
+    public String getShortName() {
+        return shortName;
+    }
+
+    public void setShortName(String shortName) {
+        this.shortName = shortName;
+    }
+
+    public Integer getStrength() {
+        return strength;
+    }
+
+    public void setStrength(Integer strength) {
+        this.strength = strength;
+    }
+
+    public String getTeamDivision() {
+        return teamDivision;
+    }
+
+    public void setTeamDivision(String teamDivision) {
+        this.teamDivision = teamDivision;
+    }
+
+    public Boolean getUnavailable() {
+        return unavailable;
+    }
+
+    public void setUnavailable(Boolean unavailable) {
+        this.unavailable = unavailable;
+    }
+
+    public Integer getWin() {
+        return win;
+    }
+
+    public void setWin(Integer win) {
+        this.win = win;
+    }
+
+    public Integer getStrengthOverallHome() {
+        return strengthOverallHome;
+    }
+
+    public void setStrengthOverallHome(Integer strengthOverallHome) {
+        this.strengthOverallHome = strengthOverallHome;
+    }
+
+    public Integer getStrengthOverallAway() {
+        return strengthOverallAway;
+    }
+
+    public void setStrengthOverallAway(Integer strengthOverallAway) {
+        this.strengthOverallAway = strengthOverallAway;
+    }
+
+    public Integer getStrengthAttackHome() {
+        return strengthAttackHome;
+    }
+
+    public void setStrengthAttackHome(Integer strengthAttackHome) {
+        this.strengthAttackHome = strengthAttackHome;
+    }
+
+    public Integer getStrengthAttackAway() {
+        return strengthAttackAway;
+    }
+
+    public void setStrengthAttackAway(Integer strengthAttackAway) {
+        this.strengthAttackAway = strengthAttackAway;
+    }
+
+    public Integer getStrengthDefenceHome() {
+        return strengthDefenceHome;
+    }
+
+    public void setStrengthDefenceHome(Integer strengthDefenceHome) {
+        this.strengthDefenceHome = strengthDefenceHome;
+    }
+
+    public Integer getStrengthDefenceAway() {
+        return strengthDefenceAway;
+    }
+
+    public void setStrengthDefenceAway(Integer strengthDefenceAway) {
+        this.strengthDefenceAway = strengthDefenceAway;
+    }
+
+    public Integer getPulseId() {
+        return pulseId;
+    }
+
+    public void setPulseId(Integer pulseId) {
+        this.pulseId = pulseId;
+    }
+}
---
+++ src/main/java/com/fantasy/football/model/PlayerBasicInformation.java
@@ -0,0 +1,85 @@
+package com.fantasy.football.model;
+
+public class PlayerBasicInformation {
+    private Long code;
+    private String firstName;
+    private Long id;
+    private String secondName;
+    private Integer squadNumber;
+    private Character status;
+    private Integer teamId;
+    private Integer teamCode;
+    private String webName;
+
+    public Long getCode() {
+        return code;
+    }
+
+    public void setCode(Long code) {
+        this.code = code;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getSecondName() {
+        return secondName;
+    }
+
+    public void setSecondName(String secondName) {
+        this.secondName = secondName;
+    }
+
+    public Integer getSquadNumber() {
+        return squadNumber;
+    }
+
+    public void setSquadNumber(Integer squadNumber) {
+        this.squadNumber = squadNumber;
+    }
+
+    public Character getStatus() {
+        return status;
+    }
+
+    public void setStatus(Character status) {
+        this.status = status;
+    }
+
+    public Integer getTeamId() {
+        return teamId;
+    }
+
+    public void setTeamId(Integer teamId) {
+        this.teamId = teamId;
+    }
+
+    public Integer getTeamCode() {
+        return teamCode;
+    }
+
+    public void setTeamCode(Integer teamCode) {
+        this.teamCode = teamCode;
+    }
+
+    public String getWebName() {
+        return webName;
+    }
+
+    public void setWebName(String webName) {
+        this.webName = webName;
+    }
+}
