06be91b5393eb5c85d3c532b86c690fa5c23f822
rohan-aditeya <rohan.aditeya@gmail.com>
2024-11-30 17:46:45 +0530

    testing build with gradle

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,6 +1,17 @@
 rootProject.name = "fantasy-football-model"
 
 pluginManagement {
+
+    repositories {
+        maven {
+            url = uri("https://maven.pkg.github.com/RohanAditeya/another-framework")
+            credentials {
+                username = providers.gradleProperty("artifactory.user") as String? ?: "user"
+                password = providers.gradleProperty("artifactory.password") as String? ?: "password"
+            }
+        }
+    }
+
     resolutionStrategy {
         eachPlugin {
             if (requested.id.id == "com.framework.another.boot")
